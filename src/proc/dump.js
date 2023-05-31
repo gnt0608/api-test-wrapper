925a975eb83e89e5148a29aa1bd85d02a86cf724
gen <genki.kondou@abi-inc.co.jp>
2023-05-31 15:29:48 +0900

    dumpのlist対応

--- src/proc/dump.js
+++ src/proc/dump.js
@@ -10,19 +10,22 @@ async function main(proc) {
   if (proc.args instanceof Array) {
     return await exec(...proc.args);
   } else {
-    return await exec(proc.args["table"], proc.args["out_path"]);
+    return await exec(proc.args["tables"], proc.args["out_path"]);
   }
 }
 
-async function exec(table, out_path) {
+async function exec(tables, out_path) {
   let connector = await ServerConnect.connect(db_env());
   console.log("Start");
   try {
-    let result = await connector.executeSelect(table);
-    fs.writeFileSync(
-      path.resolve(base_dir(), out_path ? out_path : "", table + ".csv"),
-      stringify(result.rows, { header: true })
-    );
+    let table_list = tables instanceof Array ? tables : tables.split(",");
+    for (const table of table_list) {
+      let result = await connector.executeSelect(table);
+      fs.writeFileSync(
+        path.resolve(base_dir(), out_path ? out_path : "", table + ".csv"),
+        stringify(result.rows, { header: true })
+      );
+    }
   } finally {
     connector.destroy();
   }
