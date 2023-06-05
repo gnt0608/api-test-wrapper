a89dc89015957035994388b65697dddc12606242
gen <genki.kondou@abi-inc.co.jp>
2023-06-05 20:01:13 +0900

    Logger設定追加

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,22 +1,56 @@
 import winston from "winston";
-import { format } from 'winston';
+import { format } from "winston";
+import * as fs from "fs";
+import * as path from "path";
 
-const { combine, timestamp, prettyPrint, colorize, errors,  } = format;
+const { combine, timestamp, prettyPrint, colorize, errors } = format;
 class Logger {
   private logger: winston.Logger;
 
   public constructor() {
-    if (!this.logger)
+    if (!this.logger) {
+      // FIXME: 実行ディレクトリに存在するenv.jsonを取得する形になっている
+      let trans_setting = [];
+      if (fs.existsSync(path.resolve(process.cwd(), "logger.json"))) {
+        const logger_json = JSON.parse(
+          fs.readFileSync(path.resolve(process.cwd(), "logger.json")).toString()
+        );
+
+        for (let log of logger_json) {
+          trans_setting.push(
+            new winston.transports.File({
+              filename: log.name,
+              level: log.level ?? "info",
+              format: combine(
+                errors({ stack: true }), // <-- use errors format
+                timestamp(),
+                format.printf(
+                  ({ level, message, timestamp, extra }) =>
+                    `${timestamp} ${level} ${message} ${extra}`
+                )
+              ),
+            })
+          );
+        }
+      }
       this.logger = winston.createLogger({
         level: process.env.LOG_LEVEL ?? "info",
-        transports: [new winston.transports.Console()],
-        format: combine(
-          errors({ stack: true }), // <-- use errors format
-          colorize(),
-          timestamp(),
-          prettyPrint()
-        )
+        transports: [
+          ...trans_setting,
+          new winston.transports.Console({
+            format: combine(
+              errors({ stack: true }), // <-- use errors format
+              colorize(),
+              timestamp(),
+              format.printf(
+                ({ level, message, label, timestamp, extra }) =>
+                  `${timestamp} ${level} ${message} ${extra}`
+              )
+            ),
+          }),
+        ],
       });
+    }
   }
 
   public debug(message: string, extra: object = {}) {
